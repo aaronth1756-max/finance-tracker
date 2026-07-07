cdf03e343ac50be882f3ba8c532a7efcfdcca0ff
Aaron Thomas <aaronth1756@gmail.com>
2026-07-07 06:39:20 +0300

    Configure Railway2

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ $username   = getenv("MYSQLUSER") ?: "root";
 $password   = getenv("MYSQLPASSWORD") ?: "";
 $database   = getenv("MYSQLDATABASE") ?: "finance_tracker";
 $port       = getenv("MYSQLPORT") ?: 3306;
-$conn=new mysqli($servername,$username,$password,$database);
+$conn=new mysqli($servername,$username,$password,$database,$port);
 if($conn->connect_error){
     die("Connection failed".
     $conn->connect_error);
